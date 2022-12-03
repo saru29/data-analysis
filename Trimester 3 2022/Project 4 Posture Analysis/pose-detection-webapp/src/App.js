b2ae3e03419b64a038d5ee870d3ded61bb6804dd
Quintin Xu <xuqunlele@gmail.com>
2022-12-03 12:13:17 +1100

    [Project 4 - Post Detection Webapp] 1. add initial React app. 2. add initial pacakages like eslint, prettier and mui

---
+++ Trimester 3 2022/Project 4 Posture Analysis/pose-detection-webapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        This is a pose detection web app!
+      </header>
+    </div>
+  )
+}
+
+export default App
