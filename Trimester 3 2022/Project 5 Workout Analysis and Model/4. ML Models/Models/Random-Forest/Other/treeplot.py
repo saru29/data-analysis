afbdb082c829d57c68c3ab920f4d91f8af4874f5
marktelley <68144143+marktelley@users.noreply.github.com>
2022-12-15 15:20:12 +1100

    Random Forest

---
+++ Trimester 3 2022/Project 5 Workout Analysis and Model/4. ML Models/Models/Random-Forest/Other/treeplot.py
@@ -0,0 +1,10 @@
+
+#
+# Plot Tree
+# fn= selection
+# fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (4,4), dpi=800)
+# tree.plot_tree(reg_rf.estimators_[0],
+#                feature_names = fn,
+#                filled = True);
+# fig.savefig('rf_individualtree.png')
+# plotshow(True)
