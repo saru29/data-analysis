b70ee5a1f4bf1d820ad2b841cc362eeb5e1ae8e7
whosthis-svg <68013561+whosthis-svg@users.noreply.github.com>
2023-04-28 16:08:10 +1000

    Add files via upload

--- Trimester_1_2023/Project 20 Posture Analysis/pose.py
+++ Trimester_1_2023/Project 20 Posture Analysis/pose.py
@@ -4,7 +4,8 @@ import cv2
 import numpy as np
 import tensorflow as tf
 import mediapipe as mp
-
+import json
+import datetime
 
 
 # Initialize Mediapipe parameters
@@ -16,6 +17,81 @@ mp_pose = mp.solutions.pose
 cap = cv2.VideoCapture(1)
 
 
+#Allow recording
+#fourcc = cv.VideoWriter_fourcc(*'MJPG')
+#out = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
+workout_data = []
+# Function to save workout data to a file
+def save_workout_data(workout_data):
+    current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = f"workout_{current_time}.json"
+    with open(file_name, "w") as f:
+        json.dump(workout_data, f)
+def perform_cooldown():
+    # Add code here to perform cooldown exercises
+    quad_stretch()
+    forward_fold()
+    spinal_twist()
+    hamstring_stretch()
+    child_pose()
+    shoulder_chest_stretch()
+def quad_stretch():
+    # Add code here to perform quad stretch
+    pass
+
+def forward_fold():
+    # Add code here to perform forward fold
+    pass
+
+def spinal_twist():
+    # Add code here to perform spinal twist
+    pass
+
+def hamstring_stretch():
+    # Add code here to perform hamstring stretch
+    pass
+
+def child_pose():
+    # Add code here to perform child's pose
+    pass
+
+def shoulder_chest_stretch():
+    # Add code here to perform shoulder and chest stretch
+    pass
+# Perform pre-workout stretching
+def perform_pre_workout_stretching():
+    # Cycling-oriented pre-stretches
+    # Perform each stretch for the recommended duration
+    hip_flexor_stretch()
+    hamstring_stretch()
+    quadriceps_stretch()
+    calf_stretch()
+    lower_back_stretch()
+    upper_body_stretch()
+
+def hip_flexor_stretch():
+    # Add code here to perform hip flexor stretch
+    pass
+
+def hamstring_stretch():
+    # Add code here to perform hamstring stretch
+    pass
+
+def quadriceps_stretch():
+    # Add code here to perform quadriceps stretch
+    pass
+
+def calf_stretch():
+    # Add code here to perform calf stretch
+    pass
+
+def lower_back_stretch():
+    # Add code here to perform lower back stretch
+    pass
+
+def upper_body_stretch():
+    # Add code here to perform upper body stretch
+    pass
 def calculate_angle(a,b,c):
     a = np.array(a) # First
     b = np.array(b) # Mid
@@ -26,6 +102,18 @@ def calculate_angle(a,b,c):
     
     if angle >180.0:
         angle = 360-angle
+        
+    return angle 
+
+def get_non_absolute(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    
         
     return angle 
 
@@ -44,13 +132,26 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         # Recolor back to BGR
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            
-            
+
+         
+        #recolor image to gray
+        gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        
+        # Load the Haar Cascade Classifier for human detection
+        human_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+        # Detect the humans in the image
+        humans = human_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=2)
+
+        print(humans)
+
+
         try:
             
             greenzone=(51,212,75)
             redzone=(75,51,212)
-            landmarks=results.pose_landmarks.landmark
+            landmarks=results.pose_landmaqrks.landmark
         
             # Analyze body position
             
@@ -111,7 +212,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             kneeangle=calculate_angle(hip,knee,heel)
             
             
-            #add an up/downstroke check here
+            
             if kneeangle >125 and kneeangle<155:
                 cv2.putText(image, str(round(kneeangle)), 
                             tuple(np.multiply(knee, [640, 480]).astype(int)), 
@@ -122,6 +223,58 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                             tuple(np.multiply(knee, [640, 480]).astype(int)), 
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (redzone), 2, cv2.LINE_AA)
                                    
+            #if the left heel is ahead of the right heel were in a downstroke
+            rightheel=[landmarks[mp_pose.PoseLandmark.RIGHT_HEEL.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HEEL.value].y]
+            leftheel=heel
+            lefttoe=[landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].y]
+            pedalpoint=[landmarks[mp_pose.PoseLandmark.LEFT_HEEL.value].x,landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].y]
+            
+            
+            if leftheel[0]>rightheel[0]:
+             downstroke=True
+           
+            elif leftheel[0]<rightheel[0]:
+             downstroke=False
+             
+             
+            ballangle=get_non_absolute(leftheel,lefttoe,pedalpoint)
+            
+            
+            
+            
+        #     # Analyze pedalling technique
+            if not downstroke:
+                if ballangle >15  and ballangle<25:
+                        cv2.putText(image, str(round(ballangle)), 
+                        tuple(np.multiply(leftheel, [640, 480]).astype(int)), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (greenzone), 2, cv2.LINE_AA)
+                
+                else:
+                        cv2.putText(image, str(round(ballangle)), 
+                        tuple(np.multiply(leftheel, [640, 480]).astype(int)), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (redzone), 2, cv2.LINE_AA)
+                
+            else:
+                if ballangle <5 and ballangle>15:
+                        cv2.putText(image, str(round(ballangle)), 
+                        tuple(np.multiply(leftheel, [640, 480]).astype(int)), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (greenzone), 2, cv2.LINE_AA)
+                else:
+                        cv2.putText(image, str(round(ballangle)), 
+                        tuple(np.multiply(leftheel, [640, 480]).astype(int)), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (redzone), 2, cv2.LINE_AA)
+                    
+                    
+        #     # Analyze aerodynamics
+       
+        # Draw the outlines around the human
+            for (x, y, w, h) in humans:
+              cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+          
+
+            
+                        
+
 
         except:
             pass
@@ -130,27 +283,10 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(127,79,209),thickness=2,circle_radius=2),
                                 mp_drawing.DrawingSpec(color=(213,161,54),thickness=2,circle_radius=2))
-        cv2.imshow('Video Feed',image)
+        
+        cv2.imshow('Posture Analysis',image)
+        
 
-
-         #if the left heel is ahead of the right heel were in an upstroke
-        rightheel=heel=[landmarks[mp_pose.PoseLandmark.LEFT_HEEL.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HEEL.value].y]
-
-    #     # Analyze pedalling technique
-        if stroke!='down':
-            if ballangle >15  and ballangle<25:
-                #greenzone
-            else:
-                #redzone
-        else:
-            if ballangle <-5 and ballangle>-15:
-                #greenzone
-            else:
-                #redzone
-                
-                
-    #     # Analyze aerodynamics
-           ##contour detection+transluscent image overlay+pose estimation
 
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
